5215a1b2f57f173d312e0c5be919ea21b2fb3c87
sandipdolar <sandipdolar221@gmail.com>
2026-04-02 14:10:57 +0530

    files are modified

---
+++ 2. color-palette-generator/script.js
@@ -0,0 +1,98 @@
+const generateBtn = document.getElementById("generate-btn");
+const paletteContainer = document.querySelector(".palette-container");
+
+generateBtn.addEventListener("click", generatePalette);
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+        e.preventDefault();
+        generatePalette();
+    }
+});
+
+document.addEventListener("keypress", (e) => {
+    if (e.code === "Enter") {
+        e.preventDefault();
+        generatePalette();
+    }
+});
+
+paletteContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("copy-btn")) {
+        const hexValue = e.target.previousElementSibling.textContent;
+
+        navigator.clipboard
+            .writeText(hexValue)
+            .then(() => showCopySuccess(e.target))
+            .catch((err) => console.log(err));
+    } else if (e.target.classList.contains("color")) {
+        const hexValue = e.target.nextElementSibling.querySelector(".hex-value").textContent;
+
+        navigator.clipboard
+            .writeText(hexValue)
+            .then(() => showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn")))
+            .catch((err) => console.log(err));
+    }
+});
+
+function showCopySuccess(element) {
+    element.classList.remove("far", "fa-copy");
+    element.classList.add("fas", "fa-check");
+
+    element.style.color = "#48bb78";
+
+    const tooltip = document.createElement("span");
+    tooltip.textContent = "Copied!";
+    tooltip.classList.add("tooltip");
+
+    element.parentElement.appendChild(tooltip);
+
+    // show animation
+    setTimeout(() => {
+        tooltip.classList.add("show");
+    }, 10);
+
+    // remove after 1.5 sec
+    setTimeout(() => {
+        tooltip.remove();
+
+        element.classList.remove("fas", "fa-check");
+        element.classList.add("far", "fa-copy");
+        element.style.color = "";
+    }, 1500);
+}
+
+function generatePalette() {
+    const colors = [];
+
+    for (let i = 0; i < 5; i++) {
+        colors.push(generateRandomColor());
+    }
+
+    updatePaletteDisplay(colors);
+}
+
+function generateRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+function updatePaletteDisplay(colors) {
+    const colorBoxes = document.querySelectorAll(".color-box");
+
+    colorBoxes.forEach((box, index) => {
+        const color = colors[index];
+        const colorDiv = box.querySelector(".color");
+        const hexValue = box.querySelector(".hex-value");
+
+        colorDiv.style.backgroundColor = color
+        hexValue.textContent = color;
+    })
+}
+
+generatePalette();
