cd377da7578dbf6b600455ff4739167c4abb8cab
sandipdolar <sandipdolar221@gmail.com>
2026-04-04 23:30:19 +0530

    Files midified

--- 4. Expense-Tracker/script.js
+++ 4. Expense-Tracker/script.js
@@ -50,7 +50,7 @@ function createTransactionElement(transaction) {
 
     li.innerHTML = `
         <span>${transaction.description}</span>
-        <span>${transaction.amount}
+        <span>${formatCurrency(transaction.amount)}
             <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
         </span>
     `
@@ -60,13 +60,34 @@ function createTransactionElement(transaction) {
 function updateSummary() {
     const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
 
-    const income = transactions.filter(transaction.amount > 0)
+    const income = transactions
+        .filter((transaction) => transaction.amount > 0)
         .reduce((acc, transaction) => acc + transaction.amount, 0);
-    const expanse = transactions.filter(transaction.amount < 0)
+    const expanse = transactions.
+        filter((transaction) => transaction.amount < 0)
         .reduce((acc, transaction) => acc + transaction.amount, 0);
 
     // update ui
-    balanceEl.textContent = balance;
-    incomeAmt.textContent = income;
-    expenseAmt.textContent = expanse;
+    balanceEl.textContent = formatCurrency(balance);
+    incomeAmt.textContent = formatCurrency(income);
+    expenseAmt.textContent = formatCurrency(expanse);
 }
+
+function formatCurrency(Number) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "INR"
+    }).format(Number);
+}
+
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id != id);
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    updateTransactionList();
+    updateSummary();
+}
+
+updateTransactionList();
+updateSummary();
