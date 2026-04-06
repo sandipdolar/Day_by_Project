cb3a01a0fe088c41333c8351af873e06db62b594
sandipdolar <sandipdolar221@gmail.com>
2026-04-06 23:38:45 +0530

    project modified

--- 6. form-validator/script.js
+++ 6. form-validator/script.js
@@ -5,45 +5,83 @@ const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const isRequiredValid = checkRequired([userName, email, password, confirmPassword]);
 
-    let isFormValid =  isRequiredValid();
+    let isFormValid = isRequiredValid;
 
-    if(isFormValid){
-        const isUsernameValid = checkLength(userName,3,15);
+    if (isRequiredValid) {
+        const isUsernameValid = checkLength(userName, 3, 15);
         const isEmailValid = checkEmail(email);
-        const isPasswordValid = checkLength(password,6,25);
-        const idPasswordsMatch = checkPasswordMatch(password, confirmPassword);
+        const isPasswordValid = checkLength(password, 6, 25);
+        const isPasswordsMatch = checkPasswordMatch(password, confirmPassword);
+
+        isFormValid = isUsernameValid && isEmailValid && isPasswordValid && isPasswordsMatch;
+    }
+    if (isFormValid) {
+        alert("Registration Successful!");
+        form.reset();
+
+        document.querySelectorAll(".form-group").forEach(group => {
+            group.className = "form-group";
+        });
     }
 });
 
-function checkLength(){
-    
+function checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords do not match")
+        return false;
+    }
+    showSuccess(input2);
+    return true;
 }
 
-function checkRequired(inputArray){
+function checkEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(email.value.trim())) {
+        showSuccess(email);
+        return true;
+    } else {
+        showError(email, "Email is not valid.");
+        return false;
+    }
+}
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${formatFieldName(input)} must be at least ${min} characters.`);
+        return false;
+    } else if (input.value.length > max) {
+        showError(input, `${formatFieldName(input)} must be less than ${max} characters.`);
+        return false;
+    } else {
+        showSuccess(input);
+        return true;
+    }
+}
+
+function checkRequired(inputArray) {
     let isValid = true;
 
     inputArray.forEach(input => {
 
-        if(input.value.trim() === ""){
-            showError(input, `${formatFileName(input)} is required`);
+        if (input.value.trim() === "") {
+            showError(input, `${formatFieldName(input)} is required`);
             isValid = false;
-        }else{
-            showSuccess(input);
         }
-    })
+    });
     return isValid;
-}
+};
 
-function formatFileName(input){
+function formatFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-function showError(input, message){
+function showError(input, message) {
     const formGroup = input.parentElement;
     formGroup.className = "form-group error";
 
@@ -51,7 +89,7 @@ function showError(input, message){
     small.innerText = message;
 }
 
-function showSuccess(input, ){
+function showSuccess(input) {
     const formGroup = input.parentElement;
     formGroup.className = "form-group success";
 
